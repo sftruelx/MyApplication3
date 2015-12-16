c5d4c7da1986c49cdf1514edca8cd5c262c3b0b5
sftruelx <truelx@outlook.com>
2015-12-16 12:14:11 +0800

    fgasfdgasdgasdfsadfasdfgggg

---
+++ app/src/main/java/com/example/larry/myapplication/media/MediaController.java
@@ -0,0 +1,55 @@
+package com.example.larry.myapplication.media;
+
+import android.media.MediaMetadata;
+import android.media.session.PlaybackState;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by 067231 on 2015/12/16.
+ *          播放器控制类
+ */
+public class MediaController {
+
+
+
+
+
+    public static abstract class Callback {
+        /**
+         * Override to handle the session being destroyed. The session is no
+         * longer valid after this call and calls to it will be ignored.
+         */
+        public void onSessionDestroyed() {
+        }
+
+        /**
+         * Override to handle custom events sent by the session owner without a
+         * specified interface. Controllers should only handle these for
+         * sessions they own.
+         *
+         * @param event  The event from the session.
+         * @param extras Optional parameters for the event, may be null.
+         */
+        public void onSessionEvent(@NonNull String event, @Nullable Bundle extras) {
+        }
+
+        /**
+         * Override to handle changes in playback state.
+         *
+         * @param state The new playback state of the session
+         */
+        public void onPlaybackStateChanged(@NonNull PlaybackState state) {
+        }
+
+        /**
+         * Override to handle changes to the current metadata.
+         *
+         * @param metadata The current metadata for the session or null if none.
+         * @see MediaMetadata
+         */
+        public void onMetadataChanged(@Nullable MediaMetadata metadata) {
+        }
+    }
+}
---
+++ app/src/main/java/com/example/larry/myapplication/media/media.java
@@ -0,0 +1,7 @@
+package com.example.larry.myapplication.media;
+
+/**
+ * Created by 067231 on 2015/12/16.
+ */
+public class media {
+}
