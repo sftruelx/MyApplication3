a1e7e55e022c913d772648797eb4f34be879591e
sftruelx <truelx@outlook.com>
2015-12-18 16:59:35 +0800

    hjbgkjhkjhkj  kjh kjk hkj hkhkjhkjhl;ioyh

--- app/src/main/java/com/example/larry/myapplication/NotifactionActivity.java
+++ app/src/main/java/com/example/larry/myapplication/NotifactionActivity.java
@@ -133,6 +133,60 @@ public class NotifactionActivity extends Activity {
         //注销广播
     }
 
+/*
+    public void showButtonNotify(){
+        NotificationCompat.Builder mBuilder = new Builder(this);
+        RemoteViews mRemoteViews = new RemoteViews(getPackageName(), R.layout.view_custom_button);
+        mRemoteViews.setImageViewResource(R.id.custom_song_icon, R.drawable.sing_icon);
+        //API3.0 以上的时候显示按钮，否则消失
+        mRemoteViews.setTextViewText(R.id.tv_custom_song_singer, &quot;周杰伦&quot;);
+        mRemoteViews.setTextViewText(R.id.tv_custom_song_name, &quot;七里香&quot;);
+        //如果版本号低于（3。0），那么不显示按钮
+        if(BaseTools.getSystemVersion() &lt;= 9){
+            mRemoteViews.setViewVisibility(R.id.ll_custom_button, View.GONE);
+        }else{
+            mRemoteViews.setViewVisibility(R.id.ll_custom_button, View.VISIBLE);
+        }
+        //
+        if(isPlay){
+            mRemoteViews.setImageViewResource(R.id.btn_custom_play, R.drawable.btn_pause);
+        }else{
+            mRemoteViews.setImageViewResource(R.id.btn_custom_play, R.drawable.btn_play);
+        }
+        //点击的事件处理
+        Intent buttonIntent = new Intent(ACTION_BUTTON);
+		*/
+/* 上一首按钮 *//*
+
+        buttonIntent.putExtra(INTENT_BUTTONID_TAG, BUTTON_PREV_ID);
+        //这里加了广播，所及INTENT的必须用getBroadcast方法
+        PendingIntent intent_prev = PendingIntent.getBroadcast(this, 1, buttonIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        mRemoteViews.setOnClickPendingIntent(R.id.btn_custom_prev, intent_prev);
+		*/
+/* 播放/暂停  按钮 *//*
+
+        buttonIntent.putExtra(INTENT_BUTTONID_TAG, BUTTON_PALY_ID);
+        PendingIntent intent_paly = PendingIntent.getBroadcast(this, 2, buttonIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        mRemoteViews.setOnClickPendingIntent(R.id.btn_custom_play, intent_paly);
+		*/
+/* 下一首 按钮  *//*
+
+        buttonIntent.putExtra(INTENT_BUTTONID_TAG, BUTTON_NEXT_ID);
+        PendingIntent intent_next = PendingIntent.getBroadcast(this, 3, buttonIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        mRemoteViews.setOnClickPendingIntent(R.id.btn_custom_next, intent_next);
+
+        mBuilder.setContent(mRemoteViews)
+                .setContentIntent(getDefalutIntent(Notification.FLAG_ONGOING_EVENT))
+                .setWhen(System.currentTimeMillis())// 通知产生的时间，会在通知信息里显示
+                .setTicker(&quot;正在播放&quot;)
+        .setPriority(Notification.PRIORITY_DEFAULT)// 设置该通知优先级
+                .setOngoing(true)
+                .setSmallIcon(R.drawable.sing_icon);
+        Notification notify = mBuilder.build();
+        notify.flags = Notification.FLAG_ONGOING_EVENT;
+        mNotificationManager.notify(notifyId, notify);
+    }
+*/
 
     public class MsgReceiver extends BroadcastReceiver {
         @Override
