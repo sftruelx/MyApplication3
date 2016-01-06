3233c5b06dc21f14a9cf299d67a051256d238288
truelx <truelx@163.com>
2016-01-06 21:43:18 +0800

    New changelist  mn,bn,mbn,mnb,mn,m

--- app/src/main/java/com/example/larry/myapplication/utils/AlbumArtCache.java
+++ app/src/main/java/com/example/larry/myapplication/utils/AlbumArtCache.java
@@ -20,6 +20,17 @@ import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.util.LruCache;
 
+import com.android.volley.manager.LoadController;
+import com.android.volley.manager.RequestManager;
+import com.example.larry.myapplication.entity.Classify;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 
 /**
  * Implements a basic cache of album arts, with async loading support.
@@ -27,21 +38,20 @@ import android.util.LruCache;
 public final class AlbumArtCache {
     private static final String TAG = LogHelper.makeLogTag(AlbumArtCache.class);
 
+    private LoadController loadControler = null;
+
+
     private static final int MAX_ALBUM_ART_CACHE_SIZE = 12*1024*1024;  // 12 MB
     private static final int MAX_ART_WIDTH = 800;  // pixels
     private static final int MAX_ART_HEIGHT = 480;  // pixels
 
-    // Resolution reasonable for carrying around as an icon (generally in
-    // MediaDescription.getIconBitmap). This should not be bigger than necessary, because
-    // the MediaDescription object should be lightweight. If you set it too high and try to
-    // serialize the MediaDescription, you may get FAILED BINDER TRANSACTION errors.
     private static final int MAX_ART_WIDTH_ICON = 128;  // pixels
     private static final int MAX_ART_HEIGHT_ICON = 128;  // pixels
 
     private static final int BIG_BITMAP_INDEX = 0;
     private static final int ICON_BITMAP_INDEX = 1;
 
-    private final LruCache<String, Bitmap[]> mCache;
+    private final LruCache<String, ArrayList<Classify>> mCache;
 
     private static final AlbumArtCache Instance = new AlbumArtCache();
 
@@ -50,81 +60,27 @@ public final class AlbumArtCache {
     }
 
     private AlbumArtCache() {
-
+        LogHelper.i(TAG,"new AlbumArtCache");
         // Holds no more than MAX_ALBUM_ART_CACHE_SIZE bytes, bounded by maxmemory/4 and
         // Integer.MAX_VALUE:
         int maxSize = Math.min(MAX_ALBUM_ART_CACHE_SIZE,
             (int) (Math.min(Integer.MAX_VALUE, Runtime.getRuntime().maxMemory()/4)));
-        mCache = new LruCache<String, Bitmap[]>(maxSize) {
+        mCache = new LruCache<String,  ArrayList<Classify>>(maxSize) {
             @Override
-            protected int sizeOf(String key, Bitmap[] value) {
-                return value[BIG_BITMAP_INDEX].getByteCount()
-                    + value[ICON_BITMAP_INDEX].getByteCount();
+            protected int sizeOf(String key, ArrayList<Classify> value) {
+                return value.size()*1280;
             }
         };
     }
 
-    public Bitmap getBigImage(String artUrl) {
-        Bitmap[] result = mCache.get(artUrl);
-        return result == null ? null : result[BIG_BITMAP_INDEX];
+    public ArrayList<Classify> getClassifies(String artUrl) {
+        ArrayList<Classify> result = mCache.get(artUrl);
+        return result == null ? null : result;
     }
 
-    public Bitmap getIconImage(String artUrl) {
-        Bitmap[] result = mCache.get(artUrl);
-        return result == null ? null : result[ICON_BITMAP_INDEX];
-    }
-    public void fetch(final String artUrl, final FetchListener listener) {
-        // WARNING: for the sake of simplicity, simultaneous multi-thread fetch requests
-        // are not handled properly: they may cause redundant costly operations, like HTTP
-        // requests and bitmap rescales. For production-level apps, we recommend you use
-        // a proper image loading library, like Glide.
-        Bitmap[] bitmap = mCache.get(artUrl);
-        if (bitmap != null) {
-            LogHelper.d(TAG, "getOrFetch: album art is in cache, using it", artUrl);
-            listener.onFetched(artUrl, bitmap[BIG_BITMAP_INDEX], bitmap[ICON_BITMAP_INDEX]);
-            return;
-        }
-        LogHelper.d(TAG, "getOrFetch: starting asynctask to fetch ", artUrl);
-
-        new AsyncTask<Void, Void, Bitmap[]>() {
-            @Override
-            protected Bitmap[] doInBackground(Void[] objects) {
-                Bitmap[] bitmaps;
-                try {
-//                    ImageLoader imageLoader = ImageLoader.getInstance();
-//                    // 将图片显示任务增加到执行池，图片将被显示到ImageView当轮到此ImageView
-//                    Bitmap bitmap = imageLoader.loadImageSync(artUrl);
-                    LogHelper.i(TAG,"imageLoader.loadImageSync" + artUrl);
-                    Bitmap bitmap = BitmapHelper.fetchAndRescaleBitmap(artUrl,
-                        MAX_ART_WIDTH, MAX_ART_HEIGHT);
-                    Bitmap icon = BitmapHelper.scaleBitmap(bitmap,
-                        MAX_ART_WIDTH_ICON, MAX_ART_HEIGHT_ICON);
-                    bitmaps = new Bitmap[] {bitmap, icon};
-                    mCache.put(artUrl, bitmaps);
-                } catch (Exception e) {
-                    return null;
-                }
-                LogHelper.d(TAG, "doInBackground: putting bitmap in cache. cache size=" +
-                    mCache.size());
-                return bitmaps;
-            }
-
-            @Override
-            protected void onPostExecute(Bitmap[] bitmaps) {
-                if (bitmaps == null) {
-                    listener.onError(artUrl, new IllegalArgumentException("got null bitmaps"));
-                } else {
-                    listener.onFetched(artUrl,
-                        bitmaps[BIG_BITMAP_INDEX], bitmaps[ICON_BITMAP_INDEX]);
-                }
-            }
-        }.execute();
+    public void putClassify(String artUrl,  ArrayList<Classify> classifies){
+        mCache.put(artUrl, classifies);
     }
 
-    public static abstract class FetchListener {
-        public abstract void onFetched(String artUrl, Bitmap bigImage, Bitmap iconImage);
-        public void onError(String artUrl, Exception e) {
-            LogHelper.e(TAG, e, "AlbumArtFetchListener: error while downloading " + artUrl);
-        }
-    }
+
 }
---
+++ app/src/main/java/com/example/larry/myapplication/utils/AppUrl.java
@@ -0,0 +1,9 @@
+package com.example.larry.myapplication.utils;
+
+/**
+ * Created by Larry on 2016/1/5.
+ */
+public class AppUrl {
+    public static final String webUrl = "http://222.125.193.119:8080/my_app/pic/";
+    public static final String testUrl = "http://222.125.193.119:8080/my_app/client/test";
+}
